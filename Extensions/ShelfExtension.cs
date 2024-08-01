8ef2483def9a9486a40f330f9c40e99048ac0aac
Darkmift <darkmift@gmail.com>
2024-08-01 20:11:10 +0300

    Add Shelf extension methods for space calculation
    Introduced a new namespace `MyLibrary.Extensions` and created a static class `ShelfExtension`. Added two extension methods for the `Shelf` class:
    - `FreeSpace`: Calculates the remaining space on the shelf.
    - `HasEnoughSpace`: Checks if a new book can fit on the shelf based on its height and remaining space.

---
+++ Extensions/ShelfExtension.cs
@@ -0,0 +1,27 @@
+﻿using MyLibrary.Models;
+
+namespace MyLibrary.Extensions;
+
+public static class ShelfExtension
+{
+    public static double FreeSpace(this Shelf shelf) => (double)(shelf.Width - shelf.Books.Sum(b => b.Thickness));
+
+    /// <summary>
+    /// a method that takes in this shelf and book 
+    /// and returns a boolean indicating if the shelf has enough space for the book 
+    /// </summary>
+    /// <param name="shelf"></param>
+    /// <param name="book"></param>
+    /// <returns></returns>
+    public static bool HasEnoughSpace(this Shelf shelf, Book book)
+    {
+        if (book.Height > shelf.Height)
+        {
+            return false;
+        }
+
+        var totalThickness = shelf.FreeSpace() + (double)book.Thickness;
+        return totalThickness <= (double)shelf.Width;
+    }
+}
+
