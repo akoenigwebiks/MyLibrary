27c3edaecc67fa326901608180da2996c271344d
Darkmift <darkmift@gmail.com>
2024-08-01 20:11:24 +0300

    Refactor library and shelf classes in VMAddBook
    Replaced `LibraryWithShelves` with `VMLibraryWithShelves` in `VMAddBook.cs`.
    Added `VMLibraryWithShelves` class in `VMLibraryWithShelves.cs` with properties for `LibraryId`, `Name`, and a list of `VMShelfFreeSpaceAndCount`.
    Introduced `VMShelfFreeSpaceAndCount` class in `VMShelfFreeSpaceAndCount.cs` to represent shelf data including `BookCount` and `FreeWidth`.
    Updated `VMAddBook` class to use `VMLibraryWithShelves`.

--- ViewModels/VMAddBook.cs
+++ ViewModels/VMAddBook.cs
@@ -1,17 +1,8 @@
-﻿using MyLibrary.Models;
-
-namespace MyLibrary.ViewModels;
-
-public class LibraryWithShelves
-{
-    public int LibraryId { get; set; }
-    public string Name { get; set; }
-    public List<Shelf> Shelves { get; set; }
-}
+﻿namespace MyLibrary.ViewModels;
 
 public class VMAddBook
 {
-    public List<LibraryWithShelves> LibrariesWithShelves { get; set; }
+    public List<VMLibraryWithShelves> LibrariesWithShelves { get; set; }
     public string Title { get; set; }
     public decimal Height { get; set; }
     public decimal Thickness { get; set; }
